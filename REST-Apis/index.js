6a99bedef7a09b03895b4d3f4aa064cbdba5c9bc
Rajaryan singh <raj.aryan2518@gmail.com>
2025-08-14 18:52:28 +0530

    added id to a all the post so that it can be added individually

--- REST-Apis/index.js
+++ REST-Apis/index.js
@@ -13,14 +13,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 let posts = [
     {
+        id: "1a",
         username: 'rajaryan',
         content: "i love coding! i will become Software engineer in big MNC's like Google, Microsoft, Amazon ect."
     },
     {
+        id: '2a',
         username: 'nik_sh',
         content: "i hate coding! i love 'laal Batti' i am going for governemnt exam SSC."
     },
     {
+        id: '3a',
         username: 'vis_sh',
         content: "i love finance, and persuing MBA form NFSU GandhiNagar (gujrat)"
     },
@@ -30,15 +33,22 @@ app.get('/posts', (req, res) => {
     res.render('index.ejs', { posts })
 });
 
-app.get('/posts/:new', (req, res) => {
+app.get('/posts/new', (req, res) => {
     res.render('new.ejs');
 });
 
 app.post('/posts', (req, res) => {
     let { username, content } = req.body;
     posts.push({ username, content })
-    res.send('post request working');
+    res.redirect('/posts')
 });
+
+app.get('/posts/:id', (req, res) => {
+    let { id } = req.params;
+    let post = posts.find((p) => id === p.id);
+    res.render('show.ejs', { post });
+});
+
 
 
 
