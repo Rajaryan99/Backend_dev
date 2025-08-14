eab4fd9639e95114e9a37e933f1eb0c4f34d0dea
Rajaryan singh <raj.aryan2518@gmail.com>
2025-08-14 17:05:47 +0530

    added some styling and fevicon icon in my quora

--- REST-Apis/index.js
+++ REST-Apis/index.js
@@ -2,40 +2,36 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-const port = 5000;
+const port = 3000;
 
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-//local Database
-
 let posts = [
     {
-        username: "rajryan",
-        content: "I love coding",
+        username: 'rajaryan',
+        content: "i love coding! i will become Software engineer in big MNC's like Google, Microsoft, Amazon ect."
     },
-
     {
-        username: "Aman_gupta",
-        content: "Hard Working is the key to successs",
+        username: 'nik_sh',
+        content: "i hate coding! i love 'laal Batti' i am going for governemnt exam SSC."
     },
-
     {
-        username: "vis_sh",
-        content: "Doing MBA form NFSU",
+        username: 'vis_sh',
+        content: "i love finance, and persuing MBA form NFSU GandhiNagar (gujrat)"
     },
-
-];
+]
 
 app.get('/posts', (req, res) => {
-    res.render('index.ejs', { posts });
+    res.render('index.ejs', { posts })
 })
 
-app.listen(port, () => {
-    console.log(`Server is running on http//:localhost:${port}`);
 
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
 })
