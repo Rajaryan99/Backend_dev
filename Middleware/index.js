72457f823f56ac6090c50f787002a6055c534f9d
Rajaryan singh <raj.aryan2518@gmail.com>
2025-08-31 20:32:40 +0530

    Creating middelware

--- Middleware/index.js
+++ Middleware/index.js
@@ -6,6 +6,14 @@ app.use(() => {
     console.log('I am a middleware');
 });
 
+app.get('/', (req, res) => {
+    res.send('Hi, I am root')
+});
+
+app.get('/random', (req, res) => {
+    res.send('This is a Random Page')
+})
+
 app.listen(8000, () => {
     console.log('Server is running at port 8000')
 })
