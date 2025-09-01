2523ce7f64585ff2ca4a0ba5f34e695f9c4d1bad
Rajaryan singh <raj.aryan2518@gmail.com>
2025-09-02 01:25:51 +0530

    data modelling in mongoose

---
+++ data_modelling/models/todo.models.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const todoSchema = new mongoose.Schema(
+    {
+        content: {
+            type: String,
+            required: true,
+        },
+        complete: {
+            type: Boolean,
+            default: false,
+        },
+        createdBy:{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+        },
+        subTodos: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Subtodo"
+            }
+        ]//arrays of subTodos
+    },
+    {timestamps: true},
+);
+
+
+export const Todo = mongoose.model('Todo', todoSchema);
