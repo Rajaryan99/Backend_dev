01a26dcccf1108d42f9409d82be1c7d4a022485f
Rajaryan singh <raj.aryan2518@gmail.com>
2025-09-08 23:37:54 +0530

    customer Model for database one to many relation

---
+++ Realtionship-DB/models/customer.js
@@ -0,0 +1,63 @@
+const mongoose = require('mongoose');
+
+main().then(() => {
+    console.log('connection successful!!')
+})
+.catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/relationship');
+}
+
+const orderSchema = new mongoose.Schema({
+    items: String,
+    price: Number,
+});
+
+const customerSchema = new mongoose.Schema({
+    name: String,
+    orders: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Order"
+        },
+    ],
+});
+
+
+const Order = mongoose.model('Order', orderSchema);
+const Customer = mongoose.model("Customer", customerSchema);
+
+const addCustomer = async () => {
+    let cust1 = new Customer({
+        name: "Rahul Kumar",
+    });
+
+    let order1 = await Order.findOne({items: 'Chawmin'});
+    let order2 = await Order.findOne({items: 'Pizza'});
+
+    cust1.orders.push(order1);
+    cust1.orders.push(order2);
+
+    let result = await cust1.save();
+    console.log(result);
+
+};
+
+
+addCustomer();
+
+
+
+// const addOrder = async () => {
+//    let res =  await Order.insertMany([
+//         {items: "Chawmin", price: 120},
+//         {items: "Pizza", price: 299},
+//         {items: "Pasta", price: 149},
+//         {items: "maggie", price: 99},
+//     ]);
+
+//     console.log(res);
+// };
+
+// addOrder();
