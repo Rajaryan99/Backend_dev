f4ff1ec9c6d4894657b2ccad8b13115747f0ea49
Rajaryan singh <raj.aryan2518@gmail.com>
2025-09-10 08:38:14 +0530

    vite app

--- FullStack-basic/frontend/src/App.jsx
+++ FullStack-basic/frontend/src/App.jsx
@@ -1,14 +1,35 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [hello, setHello] = useState([]);
+
+  // useEffect(() => {
+  //   axios.get('/hello')
+  //     .then((res) => {
+  //       setHello(res.data);
+  //     })
+  //     .catch((error) => {
+  //       console.log(error)
+  //     });
+  // });
 
   return (
     <>
-     <h1>Software Developer</h1>
+      <h1>Software Developer</h1>
+      <p>Greet: {hello.length}</p>
+
+      {
+        hello.map((hello, index) => {
+          <div key={hello.id}>
+            <h3>{hello.name}</h3>
+            <h4>{hello.greet}</h4>
+          </div>
+        })
+      }
     </>
   )
 }
