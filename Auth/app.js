14023c15ca0a6772a8283e7d33b3c4f2a9f9e46d
Raj/aryan <raj.aryan2518@gmail.com>
2026-03-21 00:24:53 +0530

    DB connected successfully

--- Auth/app.js
+++ Auth/app.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import jwt from 'jsonwebtoken'
 import 'dotenv/config'
+import mongoose from 'mongoose'
 
 const app = express()
 
@@ -10,7 +11,24 @@ app.get('/', (req, res) => {
     res.send("hello World!!!!")
 })
 
-app.listen(port, () => {
-    console.log(`server  is running on http://localhost:${port}`)
-})
+
+const connectDB =  async () => {
+
+    try {
+
+        await mongoose.connect(process.env.MONGODB_URL)
+        console.log('DB Connected successfully!!')
+
+        app.listen(port, () => {
+            console.log(`server  is running on http://localhost:${port}`)
+        })
+
+        
+    } catch (error) {
+        console.error("DB connection error", error)
+    }
+}
+
+connectDB();
+
 
